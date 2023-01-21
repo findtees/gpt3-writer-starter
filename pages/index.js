04c7017a1b46c5dde5a33a9b035193d1463e7bf7
Fery Setiawan <fery@vectorplay.co.id>
2023-01-21 19:27:57 +0700

    Update index.js and add generate.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,30 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
 const [userInput, setUserInput] = useState('');
+
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
 const onUserChangedText = (event) => {
   console.log(event.target.value);
   setUserInput(event.target.value);
@@ -14,10 +38,10 @@ const onUserChangedText = (event) => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Summoning: Impure World Reincarnation</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2><center>Write a message to Milton Hyland Erickson (5 December 1901 – 25 March 1980). He was an American psychiatrist and psychologist specializing in medical hypnosis and family therapy.</center></h2>
           </div>
         </div>
         {/* Add this code here*/}
@@ -30,7 +54,7 @@ const onUserChangedText = (event) => {
           />
           {/* New code I added here */}
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a className="generate-button" onClick={callGenerateEndpoint}>
               <div className="generate">
                 <p>Generate</p>
               </div>
