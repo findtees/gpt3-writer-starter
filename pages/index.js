6bc50bbda79f98d008cecda5150675e45218417f
Fery Setiawan <fery@vectorplay.co.id>
2023-01-22 16:05:32 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,19 @@ const onUserChangedText = (event) => {
               </div>
             </a>
           </div>
+          {/* New code I added here */}
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
         </div>
       </div>
       <div className="badge-container grow">
